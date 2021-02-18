a63b70ac5e3973a7e6ed8474954e784d385f8c8b
marissachilds <marissac@stanford.edu>
2021-02-17 16:56:20 -0800

    convert COVID_simulate to a function
    also remove some redundant parameters

--- COVID_simulate.R
+++ COVID_simulate.R
@@ -9,16 +9,38 @@
 # filtering trajectories or simulate forward from t0 
 
 # the parameters that must be set for simulation are in the COVID_simulate_params.R file
-if(sys.nframe() == 0L){
-  rds.name = "output/Santa_Clara_TRUE_FALSE_2020-04-01_2021-02-08_final.Rds"
-  traj.file = "output/Santa_Clara_TRUE_FALSE_2020-04-01_2021-02-08_final_filter_traj.Rds"
-  traj.trim_date = NA # as.Date("2020-03-17") # use NA to not trim the trajectories to a date
-  seed.val = 100001
-  use.rds = TRUE
-  filtering.traj = TRUE
-}
+# if(sys.nframe() == 0L){
+#   rds.name = "output/Santa_Clara_TRUE_FALSE_2020-04-01_2021-02-08_final.Rds"
+#   traj.file = "output/Santa_Clara_TRUE_FALSE_2020-04-01_2021-02-08_final_filter_traj.Rds"
+#   traj.trim_date = NA # as.Date("2020-03-17") # use NA to not trim the trajectories to a date
+#   seed.val = 100001
+#   use.rds = TRUE
+#   filtering.traj = TRUE
+# }
 
-set.seed(seed.val)
+
+COVID_simulate <- function(rds.name, 
+                           nsim = 10, ## Number of epidemic simulations for each parameter set or filtering traj
+                           traj.file = NULL, # NULL results in not using filtering trajectories
+                           ntraj = NA, # if not NA, number of filtering trajectories to use
+                           sim_length = 28,      ## if using trajectories, how many additional days from the end of the data to run the simulation, if not using trajectories, how long total the simulation will be
+                           traj.trim_date = NA,
+                           red_shelt.t,     ## Time shelter in place changes to a reduced form (inf_iso or light), if using filtering trajectories, red_shelter.t is relative to the end of the data, otherwise its relative to when the shelter in place when into effect
+                           inf_iso = FALSE,   ## Do we ever go to infected isolation?
+                           test_and_isolate_s = NA,     ## Additional proportional reduction of severe cases under test and isolate
+                           test_and_isolate_m = NA,     ## Additional proportional reduction of mild cases under test and isolate
+                           light = FALSE, # Do we ever do to lightswitch? 
+                           red_shelt.s = NA,     ## New social dist strength after time red_shelt.t, or for light switch, social distancing level to return to when thresh_H.start exceeded 
+                           thresh_H.start = NA, ## Threshold when lightswtich turns on (when we get higher than this)
+                           thresh_H.end = NA, ## Threshold when lightswtich turns on (when we get higher than this)
+                           loglik.thresh = 2,       ## Keep parameter sets with a likelihood within top X loglik units, to only fit with MLE, use 0
+                           nparams = NA,  # if no NA, the number of parameter sets to keep
+                           seed.val = NULL, 
+                           counterfactual = FALSE,
+                           delay_days = NA,
+                           ...){     ## ...if FALSE, pick the top X by loglik to use
+                           
+if(!is.null(seed.val)) {set.seed(seed.val)}
 
 ## Required packages to run this code
 needed_packages <- c(
@@ -39,7 +61,6 @@ lapply(needed_packages, require, character.only = TRUE)
 
 ## Load the previously saved fits
  ## If COVID_fit.R was just run, use parameers already stored in the global env 
-if (use.rds) {
 prev.fit           <- readRDS(rds.name)
 variable_params    <- prev.fit[["variable_params"]] %>% 
   mutate(rowid = 1:n()) # add a row id to be able to match up with the data and covariate tables stored in data_covar and the trajectories if they get read in
@@ -48,9 +69,8 @@ dynamics_summary   <- prev.fit[["dynamics_summary"]]
 mif_traces         <- prev.fit[["mif_traces"]] 
 data_covar         <- prev.fit[["data_covar"]]
 pomp_components    <- prev.fit[["pomp_components"]]
-}
 
-if(filtering.traj){
+if(!is.null(traj.file)){
   trajs <- readRDS(traj.file)
 }
 
@@ -69,7 +89,7 @@ variable_params <- variable_params %>%
 #   )
 
 
-if (!params.all) {
+if (!is.na(nparams)) {
   ## old option to pick a random X
   # variable_params <- variable_params[sample(1:nrow(variable_params), nparams), ]
   ## new option to subset to top X
@@ -90,14 +110,16 @@ for (i in 1:nrow(variable_params)) {
                     select(beta0, Ca, alpha, mu, delta, 
                            E_init, soc_dist_level_sip) %>% unlist)
   
-  if(filtering.traj){
+  if(!is.null(traj.file)){
     
     current_traj <- trajs[[variable_params[i,]$rowid]]
     traj_times <- as.numeric(dimnames(current_traj)[[2]])
     
+    if(!is.na(ntraj)){ current_traj <- current_traj[,,1:min(dim(current_traj)[3], ntraj)]}
+    
     # if doing a counterfactual, trim the trajectories to a certain date (if that date falls before the end of the trajectories)
     if(!is.na(traj.trim_date)){
-      traj.trim_index <- which(traj_times == (data_covar[[i]]$county_data %>% filter(date == traj.trim_date) %>% pull(day) %>% first))
+      traj.trim_index <- which(traj_times == (data_covar[[variable_params[i,]$rowid]]$county_data %>% filter(date == traj.trim_date) %>% pull(day) %>% first))
       if(length(traj.trim_index) > 0){
         traj_times <- traj_times[1:traj.trim_index]
         current_traj <- current_traj[,1:traj.trim_index,]
@@ -111,7 +133,7 @@ for (i in 1:nrow(variable_params)) {
       covariate_table(day = last(traj_times) + 1:sim_length
                       ,intervention = c(
                         # continut SIP for red_shelt.t days
-                        rep(2, red_shelt.t)
+                        {if(counterfactual){c(rep(1, delay_days), rep(2, red_shelt.t - delay_days))} else {rep(2, red_shelt.t)}} 
                         # Post SIP close
                         , {if (!inf_iso & !light) { # no intervention
                           rep(0, max(sim_length - red_shelt.t, 0))
@@ -128,7 +150,7 @@ for (i in 1:nrow(variable_params)) {
                       , iso_severe_level = rep(test_and_isolate_s, sim_length)      # % of contats that severe cases maintain
                       , iso_mild_level   = rep(test_and_isolate_m, sim_length)   # % of contats that mild cases maintain
                       , soc_dist_level_wfh = {
-                        if (!inf_iso) {
+                        if (!inf_iso & !light) {
                           rep(soc_dist_level_wfh, sim_length)
                         } else { # if there is infected isolation, go to the reduced social distancing level when it goes into effect
                           c(
@@ -208,13 +230,13 @@ for (i in 1:nrow(variable_params)) {
   covid_simulate <- do.call(
     pomp, 
     # for the simulator, we don't want rinit
-    args = c({if(filtering.traj) pomp_components[names(pomp_components) != "rinit"] else pomp_components},
+    args = c({if(!is.null(traj.file)) pomp_components[names(pomp_components) != "rinit"] else pomp_components},
              list(data = data_covar[[variable_params[i,]$rowid]]$county_data, 
                   covar = intervention.forecast)
              ))
   
   # simulate, either forward from the filtering trajectory, or from t0
-  if(filtering.traj){
+  if(!is.null(traj.file)){
     SEIR.sim <- adply(.data = 1:dim(current_traj)[3], 
                       .margins = 1, 
                       .fun = function(j){
@@ -265,3 +287,6 @@ if (((i / 20) %% 1) == 0) {
 }
 }
 
+return(SEIR.sim.all)
+
+}
