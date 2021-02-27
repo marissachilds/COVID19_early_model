a87018d28d931478400a6f296fbb16129560b581
marissachilds <marissac@stanford.edu>
2021-02-27 13:04:39 -0800

    add scripts to counterfactual and infected isolation sims

---
+++ counterfactual_sims.R
@@ -0,0 +1,66 @@
+needed_packages <- c(
+  "pomp"
+  , "plyr"
+  , "dplyr"
+  , "ggplot2"
+  , "magrittr"
+  , "scales"
+  , "lubridate"
+  , "tidyr"
+  , "data.table"
+)
+
+## load packages. Install all packages that return "FALSE"
+lapply(needed_packages, library, character.only = TRUE)
+
+source("COVID_simulate.R")
+
+counterfactual_general_params <- list(
+  seed.val = 10001 
+  , loglik.thresh  = 2       ## Keep parameter sets with a likelihood within top X loglik units, to only fit with MLE, use 0
+  , rds.name = "output/fits/Santa_Clara_TRUE_FALSE_2020-04-22_2021-02-05_final.Rds"
+  , nsim = 25 
+  , ntraj = 25
+  , traj.file = "output/filtering_trajectories/Santa_Clara_TRUE_FALSE_2020-04-22_2021-02-05_final_filter_traj.Rds"
+  , sim_length = as.numeric(as.Date("2020-04-22") - as.Date("2020-03-16"))     
+  , traj.trim_date = as.Date("2020-03-16")
+  , counterfactual = TRUE
+)
+
+counterfactual_params <- list(
+  obs       = list(red_shelt.t    = 500
+                   , delay_days = 0
+                   , scenario_name = "observed")
+  , week_delay = list(red_shelt.t = 500     ## Time shelter in place changes to a reduced form (inf_iso or light), if using filtering trajectories, red_shelter.t is relative to the end of the data, otherwise its relative to when the shelter in place when into effect
+                      , delay_days = 7
+                      , scenario_name = "week_delay")
+  , iso        = list(red_shelt.t = 0
+                      , delay_days = 0
+                      , inf_iso = TRUE
+                      , test_and_isolate_m = 0.3   
+                      , test_and_isolate_s = 0.2   
+                      , red_shelt.s        = "sip"
+                      , scenario_name = "iso")
+)
+
+counterfactual_sims_D <- ldply(counterfactual_params, function(int_params){
+  do.call(COVID_simulate, 
+          args = c(counterfactual_general_params, int_params,
+                   last_date_only = TRUE)) %>% 
+    select(paramset, mif2_iter, traj_set, .id, D, date) %>% 
+    mutate(scenario = int_params[["scenario_name"]]) %>% return
+})
+
+counterfactual_sims_traj <- ldply(counterfactual_params, function(int_params){
+  do.call(COVID_simulate, 
+          args = c(counterfactual_general_params, int_params,
+                   nparams        = 1,
+                   last_date_only = FALSE)) %>% 
+    select(paramset, mif2_iter, traj_set, .id, D,  I_new_sympt, day, date) %>% 
+    mutate(scenario = int_params[["scenario_name"]]) %>% return
+})
+
+
+saveRDS(list(total_D = counterfactual_sims_D, 
+             traj = counterfactual_sims_D),
+        "output/counterfactual_sims.Rds")
---
+++ inf_iso_sims.R
@@ -0,0 +1,46 @@
+needed_packages <- c(
+  "pomp"
+  , "plyr"
+  , "dplyr"
+  , "ggplot2"
+  , "magrittr"
+  , "scales"
+  , "lubridate"
+  , "tidyr"
+  , "data.table"
+)
+
+## load packages. Install all packages that return "FALSE"
+lapply(needed_packages, library, character.only = TRUE)
+
+source("COVID_simulate.R")
+
+inf_iso_sims <-
+  expand.grid(inf_iso_level = c(0.1, 0.2, 0.3),  # this is portion of remaining contacts for isolated infected 
+              background_soc_dist = c(0.6, 0.5, 0.4, 0.3)) %>%  # this is scaling on beta, so 0.3 is most effective
+  mdply(function(inf_iso_level, background_soc_dist){
+    
+    scenario_name = paste0("iso_", inf_iso_level, "_soc_dist_", background_soc_dist)
+    print(scenario_name)
+    
+    do.call(COVID_simulate, 
+            args = list(sim_length       = as.numeric(as.Date("2021-06-30") - as.Date("2020-04-22"))      
+                        , seed.val       = 10001 
+                        , nsim           = 25      
+                        , ntraj          = 25
+                        , loglik.thresh  = 2       
+                        , test_and_isolate_s = inf_iso_level     
+                        , test_and_isolate_m = inf_iso_level
+                        , red_shelt.t        = as.numeric(as.Date("2020-05-31") - as.Date("2020-04-22")) # number of days that existing SIP should last
+                        , red_shelt.s        = background_soc_dist
+                        , rds.name  = "output/Santa_Clara_TRUE_FALSE_2020-04-22_2021-02-05_final.Rds"
+                        , traj.file = "output/Santa_Clara_TRUE_FALSE_2020-04-22_2021-02-05_final_filter_traj.Rds"
+                        , inf_iso            = TRUE
+                        , light              = FALSE
+                        , last_date_only     = TRUE)) %>% 
+      select(paramset, mif2_iter, traj_set, .id, D, date) %>% 
+      mutate(scenario = scenario_name) %>% 
+      return
+  })
+
+saveRDS(inf_iso_sims, "output/inf_iso_sims_D.Rds")
