898c29667667e21fadd543495411b0e331ec41d5
marissachilds <marissac@stanford.edu>
2021-03-31 15:10:05 -0700

    Update intervention_sims.R

--- intervention_sims.R
+++ intervention_sims.R
@@ -18,6 +18,8 @@ source("COVID_simulate.R")
 
 NPI_general_params <- list(
   sim_length       = as.numeric(as.Date("2021-06-30") - as.Date("2020-04-22"))      
+  , rds.name       = "output/Santa_Clara_TRUE_FALSE_2020-04-22_2021-02-05_final.Rds"
+  , traj.file      = "output/Santa_Clara_TRUE_FALSE_2020-04-22_2021-02-05_final_filter_traj.Rds"
   , seed.val       = 10001 
   , nsim           = 25       ## Number of epidemic simulations for each filtering trajectory
   , ntraj          = 25
@@ -55,8 +57,6 @@ intervention_traj <- ldply(NPI_intervention_params, function(int_params){
   print(int_params[["scenario_name"]])
   do.call(COVID_simulate, 
           args = c(int_params, NPI_general_params, 
-                   rds.name  = "output/fits/Santa_Clara_TRUE_FALSE_2020-04-22_2021-02-05_final.Rds",
-                   traj.file = "output/filtering_trajectories/Santa_Clara_TRUE_FALSE_2020-04-22_2021-02-05_final_filter_traj.Rds",
                    nparams        = 1, 
                    last_date_only = FALSE)) %>%
     select(paramset, mif2_iter, traj_set, .id, D, H, H_new, I, I_new_sympt, day, date) %>% 
@@ -64,13 +64,10 @@ intervention_traj <- ldply(NPI_intervention_params, function(int_params){
     return
 })
 
-intervention_summary <- ldply(NPI_intervention_params, function(int_params){
+intervention_summary <- ldply(NPI_intervention_params[-4], function(int_params){
   print(int_params[["scenario_name"]])
   do.call(COVID_simulate, 
           args = c(int_params, NPI_general_params, 
-                   rds.name  = "output/fits/Santa_Clara_TRUE_FALSE_2020-04-22_2021-02-05_final.Rds",
-                   traj.file = "output/filtering_trajectories/Santa_Clara_TRUE_FALSE_2020-04-22_2021-02-05_final_filter_traj.Rds",
-                   nparams        = 2, 
                    last_date_only = FALSE, 
                    summary_fun  = function(sims_df){
                      sims_df %>%
@@ -84,6 +81,8 @@ intervention_summary <- ldply(NPI_intervention_params, function(int_params){
                                  H_new_max = max(H_new),
                                  inf_max = max(inf_simul), 
                                  inf_max_date = date[which.max(inf_simul)],
+				 I_new_sympt_max = max(I_new_sympt), 
+				 I_new_sympt_max_date = date[which.max(I_new_sympt)],
                                  D_new_max_date = date[which.max(D_new)],
                                  .groups = "drop") %>% return}
                      )) %>%
